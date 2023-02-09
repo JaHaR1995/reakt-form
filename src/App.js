7906c9c10d7d15f0e903dc78d8ace9e85e088e27
Yakumov <dgahar.yakumov@gmail.com>
2023-02-09 14:46:00 +0300

    reakt form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [text, setText] = useState() 
+  const [textError, setTextError] = useState(true)     
+  const [addText, setAddText] = useState(false)
+  const [blur, setBlur] = useState(false)  
+  
+  const handleText =(e) =>{
+    setText(e.target.value)     
+    if(!e.target.value){
+      setTextError(true)
+    }else{
+      setTextError(false)       
+      setAddText(false)   
+    }
+  }
+                                                 
+  const handleClick = () =>{      
+    console.log(text);    
+    if(text){              
+      setAddText(true)
+    } else{
+      setAddText(false)   
+      setTextError(false)   
+      
+    }
+  }                                        
+
+    
+  const handleBlur = () =>{                 
+    if(!text){
+      setTextError(true)
+    }
+    setAddText(false) 
+    setBlur(false)   
+    
+      
+  }
+
+
+  const handleFocus =() =>{
+     setTextError(false)    
+  }
+
+                                                  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='forma'>
+      <input value={text} type="text" placeholder='Введите текст' onChange={handleText}  onBlur={handleBlur}  onFocus={handleFocus}  /> 
+      <button  disabled={text} onClick={handleClick}>Отправить</button>   
+      {(textError) && <div className='Error'>Поле не должно быть пустым</div>}  
+      {(addText && blur) && <div className='OK'>Письмо успешно отправлено</div>}   
     </div>
-  );
+  )
 }
 
-export default App;
+export default App        
